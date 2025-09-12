84791202c5cca17c0d919a05b946e36c56a35aca
Rafael Werner Kuhn <rafael.k@aluno.senai.br>
2025-09-12 16:42:58 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,299 @@
+# def cadastro():
+#   print ("Cadastro:")
+#   nome1 = input ("digite seu primeiro nome: ")
+#   nome2 = input ("digite seu segundo nome: ")
+#   idade = input ("Digite sua idade: ")
+#   print ("Seus dados:", nome1 + " " + nome2 + " " + idade, "anos")
+#   respostas1 = input ("Seus dados estão corretos?")
+#   if(respostas1 == "S"):
+#     print ("Seus dados foram salvos")
+#   else:
+#     cadastro()
+# cadastro()
+
+
+
+# print("Sua calculadora do CelestiumMC")
+# numero1 = int(input ("Digite seu primeiro numero:"))
+# numero2 = int(input ("Digite seu segundo numero:"))
+# print ("A multiplicação desses numeros é igual a", numero1 * numero2)
+# print ("Inicie o código")
+# idade = 17
+# if idade >= 18:
+#   print ("vc é maior de idade")
+# print("FILLLLLLIAM"
+# print ("Quantos anos vc tem?")
+# idade = input("qual sua idade") 
+# idade1 = 18
+# if idade1 >= 18:
+#    print ("vc é maior de idade")
+
+
+
+# print ("Suas notas")
+# notas = input("qual sua nota")
+# if notas >= ("90"):
+#    print ("A")
+# elif notas >= ("80"):
+#      print ("B")
+# elif notas >= ("70"):
+#     print("C")
+# elif notas >= ("60"):
+#     print("D")
+# presença = input ("Qual sua presença")
+# if (notas >= "80" and presença >= "30"):
+#     print ("Aprovado") 
+# else:
+#     print ("Reprovado")
+# def cadastro():
+#     print ("Login:")
+#     usuário = input ("Qual seu usuário?")
+#     senha = input ("QUal sua senha?")
+#     if (usuário == "admin" and senha == "senha123"):
+#        print ("boas-vindas")
+#     else:
+#         print ("Login inválido, tente novamente")
+#         cadastro()
+# cadastro()
+   
+
+
+# print ("CALCULADORA_2.0")
+# operação = input ("Qual operação será usada: ") 
+# numero1 = int(input ("Primeiro algarismo: "))
+# numero2 = int(input ("Segundo algarismo: "))
+# if operação == "*":
+#    print ("A multiplicação desses numeros resulta em", numero1 * numero2)
+# if operação == "+":
+#    print ("A soma desses numeros resulta em", numero1 + numero2)
+# if operação == "-":
+#    print ("A subtração desses numeros resulta em", numero1 - numero2)
+# if operação == "/":
+#    print ("A divisão desses numeros resulta em", numero1 / numero2)
+
+
+
+# a = 0
+# while True:
+#    a = a+1
+#    print(a)
+#    if a >10:
+#       print(a)
+#       break
+# print ("saiu do código")
+
+
+
+# contador = 11
+# while True:
+#       contador = contador-1
+#       print(contador)
+#       if contador == 0:   
+#          break
+# print ("Acabou o tempo")         
+
+
+
+# print ("Calculadora _3.0")
+# num1 = int(input("\n Digite o primeiro numero: "))
+# operacao = input("\n Digite a operação: \n \n Para somar: + \n Para subtraír: - \n Para multiplicar: * \n Para dividir: / \n \n Digite sua operação: ")
+# num2 = int(input(" \n Digite o segundo numero: "))
+# resultado = 0
+# if operacao == "+":
+#     resultado = num1 + num2
+# elif operacao == "-":
+#     resultado = num1 - num2
+# elif operacao == "*":
+#     resultado = num1 * num2
+# elif operacao == "/":
+#     resultado = num1 / num2
+# while True:
+#     print("\n Resultado: \n ", resultado)
+#     operacao = input("Digite a nova operação: ")
+#     num2 = float(input("Digite um numero: "))
+#     if operacao == "+":
+#       resultado = resultado + num2
+#     elif operacao == "-":
+#       resultado = resultado - num2
+#     elif operacao == "*":
+#       resultado = resultado * num2
+#     elif operacao == "/":
+#       resultado = resultado / num2
+#     else:
+#      if operacao == "s":
+#        break
+# print("operacao")
+# print ("Seu calculo chegou ao fim!")
+
+
+
+# print ("Criar Conta AMAZON \n")
+# usuario1 = input  ("\nDigite seu nome de Usúario: ")
+# senha1 = input ("Digite sua Palavra-passe: ")
+# print ("\nSeus dados foram salvos!")
+
+
+
+# def cadastro():
+#   usuario2 = input("\nEntrar conta AMAZON: ")
+#   senha2 = input ("Insira sua senha de Usúario: ")
+#   if(usuario1 + senha1 == usuario2 + senha2):
+#     ("\n")
+#     print("Seu cadastro foi salvo!")
+#     ("\n")
+#   else:
+#     print ("Seu login ou senha são inválidos, tente novamente: \n")
+#     ("\n")
+#     cadastro()
+# cadastro()
+
+
+# def Cadastro():
+#   print ("Criar conta")
+#   usuario1 = input ("\nCrie seu usúario de Login: ")
+#   senha1 = input ("Crie sua Senha: ")
+#   print ("\nseus dados foram salvos! ")
+
+#   usuario2 = input ("\nColoque seu nome de usúario: ")
+#   senha2 = input ("\nColoque sua senha: ")
+  
+#   while True: 
+  
+#     if usuario2 + senha2 == usuario1 + senha1:
+#      print("Voce logou no site!")
+#     else:
+#       print ("\nSeus dados estão incorretos, Tente novamente! ")
+#       break
+
+
+# print ("ORGANIZADOR DE NÚMEROS:")
+
+# num1 = int(input ("\nDigite o 1° número: "))
+# num2 = int(input ("\nDigite o 2° número: "))
+# num3 = int(input ("\nDigite o 3° número: "))
+# num4 = int(input ("\nDigite o 4° número: "))
+# num5 = int(input ("\nDigite o 5° número: "))
+# mudou = 0
+# while True:
+#   print (num1, num2, num3, num4, num5)
+#   if num1 > num2:
+#     temp = num2
+#     num2 = num1
+#     num1 = temp
+#     mudou = 1
+  
+#   elif num2 > num3:
+#     temp = num3
+#     num3 = num2
+#     num2 = temp
+#     mudou = 1
+  
+#   elif num3 > num4:
+#     temp = num4
+#     num4 = num3
+#     num3 = temp
+#     mudou = 1
+  
+#   elif num4 > num5:
+#     temp = num5
+#     num5 = num4
+#     num4 = temp
+#     mudou = 1
+  
+#   elif mudou == 1:
+#     mudou = 0
+    
+#   else: 
+#     break
+
+
+# print ("O número é Ímpar ou Par?!\n")
+
+# while True:
+#   num1 = int(input ("Digite um número: "))
+#   if num1%2 == 0:
+#     print ("Seu número é Par! ")
+
+#   else:
+#     print ("\nSeu número é Ímpar! ")
+
+
+
+# print ("Caixa Eletrônico: ")
+# senhaCorreta = ("6173")
+# senha = input ("\nQual sua senha? ")
+# if senha == (senhaCorreta):
+#    print ("!ACESSO LIBERADO! ")
+#    saldo = 1000
+#    while True:
+#     op = input("\nEscolha uma opção:\n1 - Ver saldo: \n2 - Depositar: \n3 - Sacar: \n4 - Sair\nDIGITE UMA OPÇÃO: ")
+#     if op == "1":
+#         print ("\nSeu saldo é: ", saldo)
+#     elif op == "2":
+#      depositar = int(input("\nDeposite seu dinheiro: "))
+#      saldo = saldo + depositar
+#      if depositar <= 0:
+#       print ("!DEPÓSITO INVÁLIDO!")
+#     elif op == "3":
+#       sacar = int(input("\nSaque seu dinheiro:"))
+#       if saldo < sacar:
+#             print ("!SALDO INSUFICIENTE!")
+#       else:
+#           if saldo >= 1:
+#            saldo = saldo - sacar
+#     elif op == "4":
+#       break
+#    print ("\n!VOCÊ SAIU DO SISTEMA!")
+# else:
+#  print ("Acesso Negado! Tente novamente:   2/3 tentativas")
+# senha = input ("\nQual sua senha? ")
+# if senha == ("6173"):
+#    print ("Acesso Liberado")
+# else: 
+#    print ("Acesso Negado! Tente novamente:   3/3 tentativas")
+#    senha = input ("\nQual sua senha? ")
+#    if senha == ("6173"):
+#     print ("Acesso Liberado")
+#    else: 
+#     print ("Acesso Negado! CARTÃO BLOQUEADO!!! ")
+
+
+# while True:
+#    def fahrenheit_para_celsius(fahrenheit):
+#       celsius = (fahrenheit - 32)*5/9
+#       return celsius
+#    temp_fahrenheit = float(input("Digite a temperatura: "))
+#    temp_celsius = fahrenheit_para_celsius(temp_fahrenheit)
+#    print("A temperatura em Celsius é: ", temp_celsius)
+
+# while True:
+#    def celsius_para_fahrenheit(celsius):
+#       fahrenheit = (celsius * 9/5) + 32
+#       return fahrenheit
+#    temp_celsius = float(input("Digite a temperatura: "))
+#    temp_fahrenheit = celsius_para_fahrenheit(temp_celsius)
+#    print("A temperatura em fahrenheit é: ", temp_fahrenheit)
+
+# def funcao1variavel(variavel):
+#    variavel = variavel + 1 
+#    return variavel
+# a = 1
+# b = funcao1variavel(a)
+# print (b)
+
+# def funcao2variavel(variavel,variavel2):
+#    temp = variavel
+#    variavel = variavel2
+#    variavel2 = temp
+#    return variavel, variavel2
+
+
+# a= 1
+# b= 2
+
+# Calculadora Fatorial
+
+
 def maquina():
   print("Máquina de Lavar:")
   iniciar = int(input("\nBaixo: 50L\nMédio: 125L\nAlto: 200L\nVoce deseja adicionar quantos Litros de Água? "))
@@ -20,20 +316,49 @@ def maquina():
     maquina()
 maquina()
 
-
-
+adicionais = []
 
 while True:
-  adi = int(input("\n1: Sabão em pó \n2: Líquido para Limpeza \n3: Tira manchas \n4: Amaciante\n5: Nada\nO que deseja adicionar na Máquina:"))
+  adi = int(input("\n1: Sabão em pó \n2: Líquido para Limpeza \n3: Tira manchas \n4: Amaciante\n5: Ver Lista\n6: Nada\nO que deseja adicionar na Máquina:"))
 
   if adi == 1:
-    print("Sabão em pó foi Adicionado")
+      adicionais.append("Sabão em Pó")
+      print("\nSabão em pó foi Adicionado")
   elif adi == 2:
-    print("Líquido para Limpeza foi Adicionado")
+      adicionais.append("Líquido para Limpeza")
+      print("\nLíquido para Limpeza foi Adicionado")
   elif adi == 3:
-    print("Tira manchas foi Adicionado")
+      adicionais.append("Tira Manchas")
+      print("\nTira manchas foi Adicionado")
   elif adi == 4:
-     print("\nAmaciante foi Adicionado")
+      adicionais.append("Amaciante")
+      print("\nAmaciante foi Adicionado")
+  elif adi == 5:
+      print("\nSua lista de adicionais é:", adicionais)
   else:
-   break
-print("A MÁQUINA FOI INICIADA!!!")
+      break
+
+print("\nA MÁQUINA FOI INICIADA!!!")
+contador = 1000
+if adi == 6:
+  contador *= 1000
+  while True:
+    contador -= 1
+    print(contador, "segundos")
+    if contador == 0:
+      break
+
+if contador == 0:
+  cent = int(input("Deseja Ativar modo de Centrifuga?\n1: Sim\n2: Não\nDigite uma opção: "))
+  if cent == 1:
+    print("\nA MÁQUINA FOI INICIADA!!!")
+contador = 100
+contador *= 100
+while True:
+    contador -= 1
+    print(contador, "segundos")
+    if contador == 0:
+      break
+    print("A MÁQUINA ESTA PRONTA!!!")
+    if cent == 2:
+      print("A MÁQUINA ESTA PRONTA")
